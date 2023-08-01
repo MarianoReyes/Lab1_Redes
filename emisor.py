292ec0afbddf5326dc2e0e433d21cfe7c4ba68c6
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-08-01 17:05:51 -0600

    cambio en crc

--- emisor.py
+++ emisor.py
@@ -2,16 +2,10 @@ import crcmod.predefined
 
 
 def crc_encode(message):
+    # Creamos un generador de CRC-32 con el polinomio estándar (0x04C11DB7)
     crc32 = crcmod.predefined.Crc('crc-32')
-    crc_value = crc32.new(message.encode()).digest()
-
-    # Convertir el valor CRC en un número entero
-    crc_int = int.from_bytes(crc_value, byteorder='big')
-
-    # Obtener los 3 bits menos significativos del valor CRC
-    crc_bits = bin(crc_int & 0b111).replace('0b', '').zfill(3)
-
-    encoded_message = message + crc_bits
+    crc_value = crc32.new(message.encode()).hexdigest()
+    encoded_message = message + crc_value
     return encoded_message
 
 
@@ -19,11 +13,17 @@ def main():
     binary_message = input("Ingrese una trama en binario (p.ej. 110101): ")
     encoded_message = crc_encode(binary_message)
 
-    # Guardar la respuesta en un archivo de texto
-    with open("respuesta.txt", "w") as file:
-        file.write(encoded_message)
+    # Separar la trama original y el valor CRC-32
+    original_message = encoded_message[:-8]
+    crc_value = encoded_message[-8:]
 
-    print("Mensaje codificado guardado en 'respuesta.txt'.")
+    # Guardar solo la trama modificada en un archivo de texto
+    with open("trama_modificada.txt", "w") as file:
+        file.write("Trama modificada: " + encoded_message + "\n")
+        file.write("Trama original: " + original_message + "\n")
+        file.write("Valor CRC-32: " + crc_value + "\n")
+
+    print("Trama modificada guardada en 'trama_modificada.txt'.")
 
 
 if __name__ == "__main__":
