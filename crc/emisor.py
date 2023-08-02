5d6482bfe37b98e136fb3f21b4b8db429d72ad6e
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-08-01 19:29:32 -0600

    crc funcional sin errores

---
+++ crc/emisor.py
@@ -0,0 +1,33 @@
+def crc_simplified(data):
+    polynomial = [1, 1, 0, 1]  # Polinomio generador: x^3 + x^2 + 1 (grado 3)
+
+    # Convertir la cadena de entrada en una lista de bits
+    bits = [int(bit) for bit in data]
+
+    # Agregar tres bits de paridad al final
+    for _ in range(3):
+        bits.append(0)
+
+    # Realizar el cálculo de CRC
+    for i in range(len(bits) - 3):
+        if bits[i] == 1:
+            for j in range(4):
+                bits[i + j] ^= polynomial[j]
+
+    # Convertir los bits a una cadena binaria
+    crc_result = ''.join(str(bit) for bit in bits[-3:])
+
+    # Devolver el mensaje en binario concatenado con la información adicional (CRC)
+    return data + crc_result
+
+# Ejemplo de uso:
+input_data = input("Ingresa una trama ej: 11010011101100\n>>")
+output_data = crc_simplified(input_data)
+
+# Guardar el resultado en un archivo
+with open("crc/inputMessage.txt", "w") as file:
+    file.write(output_data)
+
+print("Mensaje original + CRC:", output_data)
+print("El resultado se ha guardado en el archivo inputMessage.txt")
+
---
+++ crc/receptor.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+function readInputMessageFromFile(filePath) {
+  try {
+    const inputMessage = fs.readFileSync(filePath, 'utf8');
+    return inputMessage.trim(); // Eliminar espacios en blanco al principio o al final, si los hay
+  } catch (error) {
+    console.error("Error al leer el archivo:", error);
+    return null;
+  }
+}
+
+function crc_simplified(data) {
+    const polynomial = [1, 1, 0, 1]; // Polinomio generador: x^3 + x^2 + 1 (grado 3)
+  
+    // Convertir la cadena de entrada en un array de bits
+    const bits = data.split('').map(bit => parseInt(bit));
+  
+    // Realizar el cálculo de CRC
+    for (let i = 0; i < bits.length - 3; i++) {
+      if (bits[i] === 1) {
+        for (let j = 0; j < 4; j++) {
+          bits[i + j] ^= polynomial[j];
+        }
+      }
+    }
+  
+    // Verificar si se detectaron errores
+    const crc_result = bits.slice(-3);
+    const errorsDetected = crc_result.some(bit => bit === 1);
+  
+    if (!errorsDetected) {
+      // Caso: No se detectaron errores
+      return { valid: true, message: data };
+    } else {
+      // Caso: Se detectaron errores
+      // Descartar la trama
+      return { valid: false, message: "Trama descartada por detectar errores." };
+    }
+  }
+  
+  function decodeMessage(encodedMessage) {
+    const data = encodedMessage.slice(0, -3); // Eliminar los 3 bits de paridad del final
+    const crc = encodedMessage.slice(-3); // Obtener los 3 bits de paridad del final
+  
+    const result = crc_simplified(encodedMessage);
+  
+    if (result.valid) {
+      // Caso: No se detectaron errores
+      return `Trama recibida: ${data}`;
+    } else {
+      // Caso: Se detectaron errores
+      // Intentar corregir errores invirtiendo el bit que está en la posición de los bits de paridad (crc)
+      const errorPosition = parseInt(crc.join(''), 2);
+      if (errorPosition >= 0 && errorPosition < data.length) {
+        const correctedMessage = data.slice(0, errorPosition) + (1 - parseInt(data[errorPosition])) + data.slice(errorPosition + 1);
+        return `Trama corregida: ${correctedMessage}`;
+      } else {
+        return result.message; // Si la posición de error está fuera del rango, simplemente se descarta la trama.
+      }
+    }
+  }
+  
+ // Obtener el mensaje desde el archivo
+const filePath = 'crc/inputMessage.txt';
+const inputMessageFromFile = readInputMessageFromFile(filePath);
+
+if (inputMessageFromFile) {
+  // Decodificar el mensaje
+  const decodedResult = decodeMessage(inputMessageFromFile);
+  console.log(decodedResult);
+}
