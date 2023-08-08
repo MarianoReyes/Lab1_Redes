5a76e02ccba020071809fb5d8aaddecd53a0d831
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-08-08 10:49:56 -0600

    avances varios en crc

--- parte2/crc/emisor.py
+++ parte2/crc/emisor.py
@@ -1,1 +1,49 @@
-print("py")
+import socket
+import random
+import zlib
+
+
+def solicitar_mensaje():
+    mensaje = input("Ingrese el mensaje a enviar: ")
+    return mensaje
+
+
+def codificar_mensaje(mensaje):
+    mensaje_codificado = ""
+    for char in mensaje:
+        ascii_code = ord(char)
+        binary_code = bin(ascii_code)[2:].zfill(8)
+        mensaje_codificado += binary_code
+    return mensaje_codificado
+
+
+def calcular_integridad(mensaje_binario):
+    crc32 = zlib.crc32(mensaje_binario.encode()) & 0xFFFFFFFF
+    crc32_binary = bin(crc32)[2:].zfill(32)
+    return crc32_binary
+
+
+def aplicar_ruido(trama):
+    trama_con_ruido = ""
+    for bit in trama:
+        if random.random() < 0.01:  # Probabilidad de error del 1%
+            bit = "1" if bit == "0" else "0"
+        trama_con_ruido += bit
+    return trama_con_ruido
+
+
+def enviar_informacion(trama_con_ruido):
+    HOST = 'localhost'
+    PORT = 8080
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(trama_con_ruido.encode())
+
+
+mensaje = solicitar_mensaje()
+mensaje_codificado = codificar_mensaje(mensaje)
+integridad = calcular_integridad(mensaje_codificado)
+trama = mensaje_codificado + integridad
+trama_con_ruido = aplicar_ruido(trama)
+enviar_informacion(trama_con_ruido)
---
+++ parte2/crc/receptor.js
@@ -0,0 +1,54 @@
+const net = require('net');
+const zlib = require('zlib');
+const crc32 = require('crc').crc32;
+
+const server = net.createServer((socket) => {
+    let receivedData = "";
+
+    socket.on('data', (data) => {
+        receivedData += data.toString();
+    });
+
+    socket.on('end', () => {
+        const [mensajeBinario, integridad] = obtenerMensajeEIntegridad(receivedData);
+        const crc32_calculado = calcularCRC32(mensajeBinario);
+        if (crc32_calculado === integridad) {
+            const mensajeDecodificado = decodificarMensaje(mensajeBinario);
+            mostrarMensaje(mensajeDecodificado);
+        } else {
+            console.log("Error de integridad detectado en la trama recibida.");
+        }
+    });
+});
+
+function obtenerMensajeEIntegridad(trama) {
+    // La trama contiene el mensaje codificado + integridad
+    const mensajeBinario = trama.substring(0, trama.length - 32);
+    const integridad = trama.substring(trama.length - 32);
+    return [mensajeBinario, integridad];
+}
+
+function calcularCRC32(mensaje) {
+    const buffer = Buffer.from(mensaje, 'binary');
+    const crc32Value = crc32(buffer).toString(2).padStart(32, '0');
+    return crc32Value;
+}
+
+function decodificarMensaje(mensajeBinario) {
+    let mensajeDecodificado = "";
+    for (let i = 0; i < mensajeBinario.length; i += 8) {
+        const byte = mensajeBinario.substr(i, 8);
+        const asciiCode = parseInt(byte, 2);
+        const char = String.fromCharCode(asciiCode);
+        mensajeDecodificado += char;
+    }
+    return mensajeDecodificado;
+}
+
+function mostrarMensaje(mensaje) {
+    console.log("Mensaje recibido y verificado:", mensaje);
+}
+
+server.listen(8080, 'localhost', () => {
+    console.log('Receptor escuchando en el puerto 8080');
+});
